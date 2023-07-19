f50f4608788604cf4eebb3c51cd214180dbba55e
lhiuming <laaihiuming@gmail.com>
2023-07-19 21:19:33 +0800

    Add specular in reference path tracer.

--- shader/brdf.hlsl
+++ shader/brdf.hlsl
@@ -52,3 +52,93 @@ float Fd_Burley(float NoV, float NoL, float LoH, float roughness) {
     float viewScatter = F_Schlick_single(NoV, 1.0, f90);
     return lightScatter * viewScatter * (1.0 / PI);
 }
+
+
+// Lambda term in Smith masking function for isotropic GGX NDF.
+// [Heitz 2014 "Understanding the Masking-Shadowing Function", p84]
+// [Boksansky 2021 "Crash Course in BRDF Implementation", p13]
+float smith_lambda_GGX(float cos_theta, float roughness)
+{
+#if 1
+    // Reference
+    float sin_theta = sqrt(1.0f - cos_theta * cos_theta);
+    float tan_theta = sin_theta / cos_theta;
+    float a = rcp(roughness * tan_theta); // Heitz
+          a = cos_theta * rcp(roughness * sin_theta); // Jakub
+          a = cos_theta * rcp( roughness * sqrt(1.0f - cos_theta*cos_theta) ); // Jakub
+    return ( -1.0f + sqrt( 1.0f + rcp(a*a) ) ) / 2.0f;
+#else
+    // Simplified
+    float r2 = roughness * roughness;
+    float c = cos_theta;
+    return ( -1.0f + sqrt( (-r2 * c + c) * c + r2 ) / c ) / 2.0f;
+#endif
+}
+
+// Smith masking-shadowing function for isotropic GGX NDF.
+// Note that binary masking by HoV>0 and HoL>0 is ignoreed (left to calling site).
+// [Heitz 2014 "Understanding the Masking-Shadowing Function", p91]
+// [Boksansky 2021 "Crash Course in BRDF Implementation", p14]
+//   Note that this blog seems to mistabke N for H when discussing Smith model.
+float smith_G2_height_correlated_GGX(float NoL, float NoV, float roughness)
+{
+#if 1
+    // Reference
+    float lambda_L = smith_lambda_GGX(NoL, roughness);
+    float lambda_V = smith_lambda_GGX(NoV, roughness);
+    return 1.0f / (1.0f + lambda_L + lambda_V);
+#else
+    // Optimized
+    float r2 = roughness * roughness;
+    float k_L = sqrt((-r2 * NoL + NoL) * NoL + r2) / NoL; // 2 fma, 1 sqrt, 1 div
+    float k_V = sqrt((-r2 * NoV + NoV) * NoV + r2) / NoV;
+    return 2.0f / ( k_L + k_V );
+#endif
+}
+
+// Visibility with Smith masking-shadowing function for isotropic GGX NDF.
+// Vis := G / (4*NoL*NoV).
+// Divide G by 4*NoL*NoV from the full microfacet BRDF to cancel out some terms.
+// Note that binary masking by HoV>0 and HoL>0 is ignoreed (left to calling site).
+// [Lagarde 2014, "Moving Frostbite to Physically Based Rendering", p12]
+float vis_smith_G2_height_correlated_GGX(float NoL, float NoV, float roughness)
+{
+#if 0
+    // Reference
+    float G = smith_G2_height_correlated(HoL, HoV, roughness);
+    float Vis = G / (4.0f * NoL * NoV);
+    return Vis;
+#else
+    // Optimized
+    float r2 = roughness * roughness;
+    float u_L = NoV * sqrt((-r2 * NoL + NoL) * NoL + r2);
+    float u_V = NoL * sqrt((-r2 * NoV + NoV) * NoV + r2);
+    return 0.5f / (u_L + u_V);
+#endif
+}
+
+// Smith G2/G1 for isotropic GGX, only used when generate sampling from GGX VNDF.
+// Note that G1 is for Smith G1 masking function (G1(V)).
+// Note that binary masking by HoL>0 is ignoreed (left to calling site).
+// [Heitz 2014 "Importance Sampling Microfacet-Based BSDFs using the Distribution of Visible Normals", section 2]
+float smith_G2_over_G1_height_correlated_GGX(float NoL, float NoV, float roughness)
+{
+#if 0
+    // Reference 
+    float G2 = smith_G2_height_correlated_GGX(NoL, NoV, roughness);
+    float G1_V = rcp(1 + smith_lambda_GGX(NoV, roughness));
+    return G1 / G1_V;
+#elif 0
+    // Reference, alternative
+    // [Heitz 2015 "Implementing a Simple Anisotropic Rough Diffuse Material with Stochastic Evaluation", appendix A]
+    float l_V = smith_lambda_GGX(NoV, roughness);
+    float l_L = smith_lambda_GGX(NoL, roughness);
+    return (1.0f + l_V) / (1.0f + l_V + l_L);
+#else
+    // Optimized
+    float r2 = roughness * roughness;
+    float k_V = sqrt( (-r2 * NoV + NoV) * NoV + r2 ) / NoV;
+    float k_L = sqrt( (-r2 * NoL + NoL) * NoL + r2 ) / NoL;
+    return (1.0f + k_V) / (k_V + k_L);
+#endif
+}
--- shader/pathtraced_lighting.hlsl
+++ shader/pathtraced_lighting.hlsl
@@ -13,6 +13,8 @@ Reference:
 
 #define PRIMARY_RAY_JITTER 1
 #define WHITE_FURNACE 0
+#define SPECULAR_ONLY 0
+#define DIFFUSE_ONLY 0
 
 RaytracingAccelerationStructure scene_tlas;
 TextureCube<float4> skycube;
@@ -148,7 +150,7 @@ void raygen() {
     float3 view_dir = normalize(view_dir_end - view_params.view_pos);
 
     // TODO Frame Parameters
-    float exposure = 20;
+    float exposure = 5;
     float3 light_color = float3(0.7f, 0.7f, 0.6f) * PI * exposure;
 
     // Setup primary Ray
@@ -163,7 +165,7 @@ void raygen() {
 
     // Ray tracing loop
 
-    float3 debug_color = 0.0f;
+    float4 debug_color = 0.0f;
     for (int bounce = 0; bounce < MAX_BOUNCE; ++bounce) 
     {
         debug_color.r = float(bounce) / MAX_BOUNCE;
@@ -196,6 +198,11 @@ void raygen() {
             break;
         }
 
+        // Material attribute decode
+	    const float3 specular_color = lerp(float3(0.04f, 0.04f, 0.04f), hit.base_color, hit.metallic);
+	    const float3 diffuse_color = hit.base_color* (1.0f - hit.metallic);
+        const float roughness = hit.perceptual_roughness * hit.perceptual_roughness;
+
         // Add Direct Lighting
         #if !WHITE_FURNACE
         float nol_sun = dot(hit.normal_ws, view_params.sun_dir);
@@ -224,18 +231,40 @@ void raygen() {
 
             if (shadow.missed)
             {
-                // TODO specular
-                float3 brdf = hit.base_color * ONE_OVER_PI;
+                float3 brdf = 0.0f;
+
+                #if !SPECULAR_ONLY
+                // Diffuse
+                brdf += diffuse_color * ONE_OVER_PI;
+                #endif
+
+                #if !DIFFUSE_ONLY
+                // Specualr
+                float LoH = max(0.0001f, dot(view_dir, normalize(view_dir + view_params.sun_dir)));
+                float NoL = max(0.0001f, nol_sun);
+                float NoV = max(0.0001f, dot(hit.normal_ws, -ray.Direction));
+                float3 F = F_Schlick(LoH, specular_color);
+                float V = vis_smith_G2_height_correlated_GGX(NoL, NoV, roughness);
+                float D = D_GGX(LoH, roughness);
+                brdf += F * (V * D);
+                #endif
+
                 radiance += (throughput * nol_sun) * brdf * light_color;
             }
         }
         #endif
 
-        // Material attribute decode
-	    const float3 specular_color = lerp(float3(0.04f, 0.04f, 0.04f), hit.base_color, hit.metallic);
-	    const float3 diffuse_color = hit.base_color* (1.0f - hit.metallic);
 
-        // Pick brdf lobe (importance sampling the lobe with heuristics)
+#if 0
+        // Material debug
+        debug_color.rgb = diffuse_color;
+        debug_color.rgb = roughness.rrr;
+        debug_color.a = 1.0f;
+        break;
+#endif
+
+        // Pick BRDF lobe (importance sampling alomgs lobes with heuristics)
+        // NOTE: we just add diffse and specular components, thus we multiply 1/pdf to throughput 
         // ref: RTGII, ch14
         float prop_specular;
         {
@@ -247,6 +276,12 @@ void raygen() {
             prop_specular = e_spec / max(0.0000001f, e_spec + e_diff);
             // remove extreme value of prop_specular to avoid undersampling specular/diffuse
             prop_specular = clamp(prop_specular, 0.1f, 0.9f);
+#if SPECULAR_ONLY
+            prop_specular = 1.0f;
+#endif
+#if DIFFUSE_ONLY
+            prop_specular = 0.0f;
+#endif
         }
         bool brdf_is_specular = rand(rng_state) < prop_specular;
         if (brdf_is_specular)
@@ -256,35 +291,85 @@ void raygen() {
             throughput /= (1.0 - prop_specular);
         }
 
-        // Generate bounce sample direction 
-        float3 brdf_NoL_over_pdf;
-        float3 new_dir;
+        // Generate bounce sample direction, and calculate sample weight
+        float3 brdf_NoL_over_pdf; // := BRDF() * NoL / pdf, also call `sample weight`
+        float3 new_dir_local;
         float2 u = float2(rand(rng_state), rand(rng_state));
         float4 rot_to_local = get_rotation_to_z_from(hit.normal_ws);
         if (brdf_is_specular) 
         {
-            float3 prev_dir_local = rotate_point(rot_to_local, ray.Direction);
+            float3 V_local = rotate_point(rot_to_local, ray.Direction);
 
-            float3 H_local = 0.0f; // TODO
-            float3 L_local = reflect(prev_dir_local, H_local);
-            // TODO 
-            brdf_NoL_over_pdf = 0.0f;
+            // Sample a half vector (microfacet normal) from the GGX distribution of visible normals (VNDF).
+            // pdf(H) = D_visible(H) * Jacobian_refl(V, H) 
+            //        = ( G1(V) * VoH * D(H) / NoV ) * ( 1 / (4 * VoH) )
+            //        = G1(V) * D(H) / (4 * NoV)
+            // [Heitz 2014 "Importance Sampling Microfacet-Based BSDFs using the Distribution of Visible Normals", section 2]
+            float3 H_local;
+            if (roughness != 0)
+            {
+                // ref: https://github.com/boksajak/referencePT/blob/master/shaders/brdf.h#L727
+                // TODO check and compare other methods [Heitz 2018 "Sampling the GGX Distribution of Visible Normals"]
+                float2 alpha2d = roughness.xx;
+
+                float3 v_h = normalize(float3(alpha2d.x * V_local.x, alpha2d.y * V_local.y, V_local.z));
+
+                float lensq = dot(v_h.xy, v_h.xy);
+                float3 T1 = lensq > 0.0f ? float3(-v_h.y, v_h.x, 0.0f) * rsqrt(lensq) : float3(1.0f, 0.0f, 0.0f);
+                float3 T2 = cross(v_h, T1);
+
+                float r = sqrt(u.x);
+                float phi = TWO_PI * u.y;
+                float t1 = r * cos(phi);
+                float t2 = r * sin(phi);
+                float s = 0.5f * (1.0f + v_h.z);
+                t2 = lerp(sqrt(1.0f - t1 * t1), t2, s);
+
+                float3 n_h = t1 * T1 + t2 * T2 + sqrt(max(0.0f, 1.0f - t1 * t1 - t2 * t2)) * v_h;
+
+                H_local = normalize(float3(alpha2d.x * n_h.x, alpha2d.y * n_h.y, max(0.0f, n_h.z)));
+            }
+            else 
+            {
+                // perfect mirror (roughness == 0.0f) has singularity when transform the view direction to hemisphere configturation 
+                H_local = float3(0.0f, 0.0f, 1.0f);
+            }
+
+            float3 L_local = reflect(V_local, H_local);
+            new_dir_local = L_local;
+
+            float LoH = max(0.000001f, dot(L_local, H_local));
+            float3 N_local = float3(0.0f, 0.0f, 1.0f);
+            float NoL = max(0.000001f, dot(N_local, L_local)); // L_local.z
+            float NoV = max(0.000001f, dot(N_local, V_local)); // V_local.z
+
+            float3 F = F_Schlick(LoH, specular_color);
+
+            float G2_over_G1 = smith_G2_over_G1_height_correlated_GGX(NoL, NoV, roughness);
+
+            // Original formular:
+            // pdf               = G1 * D / (4 * NoV)
+            // brdf              = F * D * G2 / (4 * NoV * NoL);
+            // brdf_NoL_over_pdf = bdrf * NoL / pdf
+            //                   = F * D * G2 * NoL / (4 * NoV * NoL * pdf)
+            //                   = F * G2 / G1
+            brdf_NoL_over_pdf = F * G2_over_G1;
         }
         else
         {
             float pdf;
-            float3 dir_local = sample_hemisphere_cosine(u, pdf);
-            new_dir = rotate_point(invert_rotation(rot_to_local), dir_local);
-#if NOT_OPTIMIZE
-            float3 brdf = diffuse_color * ONE_OVER_PI;
-            float NoL = dot(hit.normal_ws, new_dir);
-            brdf_NoL_over_pdf = brdf * NoL / pdf;
-#else
-            // pdf = NoL / PI 
+            new_dir_local = sample_hemisphere_cosine(u, pdf);
+
+            // Original formular:
+            // pdf               = NoL / PI 
+            // brdf              = diffuse_color / PI
+            // brdf_NoL_over_pdf = brdf * NoL / pdf
+            //                   = diffuse_color * NoL / (PI * pdf)
+            //                   = diffuse_color
             brdf_NoL_over_pdf = diffuse_color;
-#endif
         }
-        ray.Direction = new_dir;
+        ray.Direction = rotate_point(invert_rotation(rot_to_local), new_dir_local);
+        ray.Direction = normalize(ray.Direction);
 
         // Terminate poth with zero contribution
         if (max3(brdf_NoL_over_pdf) <= 0.0f)
@@ -317,8 +402,9 @@ void raygen() {
     float3 avg_radiance = accu_radiance / pc.accumulated_count;
     rw_lighting[dispatch_id.xy] = float4(avg_radiance, 1.0f);
 
-#if 0
-    rw_lighting[dispatch_id.xy] = float4(debug_color, 1.0f);
+#if 1
+    if (debug_color.a > 0.0f)
+    rw_lighting[dispatch_id.xy] = debug_color;
 #endif
 }
 
